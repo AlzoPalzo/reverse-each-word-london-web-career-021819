aef06b570630ca33f2d18d59e75fb4c04f738dc6
Ali Madni <ali555m@hotmail.com>
2019-02-10 18:59:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(string)
   array = string.split
-  rev_array =[]
-  array.collect do |word|
-    rev_array.push(word.reverse)
-  end
-  rev_array.join(" ")
+  array.join(array.collect do |word|
+    word.reverse
+  end)
 end
